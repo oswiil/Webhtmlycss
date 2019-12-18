aa0bc258e744bf4999f75adf84f85537cbda89da
Ojcar Sánchez <jfosanchez@gmail.com>
2019-12-18 17:46:17 +0100

    static url kuai

--- src/components/regostylinks.js
+++ src/components/regostylinks.js
@@ -1,17 +1,37 @@
-var links = [
-  "http://app.kuailiandp.com/auth/register/ref/8effea01-dc72-497e-b89a-271c592db093",
-  "http://app.kuailiandp.com/auth/register/ref/a5a67f20-a8ea-4bbd-9032-e456deafb284",
-  "http://app.kuailiandp.com/auth/register/ref/d70d60e9-7704-46fa-a943-858ff5f63e72",
+import React from "react"
+
+const links = [
+  {
+    label: "registrate AQUI",
+    path:
+      "http://app.kuailiandp.com/auth/register/ref/8effea01-dc72-497e-b89a-271c592db093",
+  },
+  {
+    label: "registrate AQUI",
+    path:
+      "http://app.kuailiandp.com/auth/register/ref/a5a67f20-a8ea-4bbd-9032-e456deafb284",
+  },
+  {
+    label: "registrate AQUI",
+    path:
+      "http://app.kuailiandp.com/auth/register/ref/d70d60e9-7704-46fa-a943-858ff5f63e72",
+  },
 ]
 
-var OpenSite = function() {
+const OpenSite = () => {
   // get a random number
   var randIdx = Math.random() * links.length
-  // round it, so it can be used as array index
-  randIdx = parseInt(randIdx, 10)
+  randIdx = Math.floor(randIdx)
   // construct the link to be opened
   var random = links[randIdx]
 
-  return random
+  console.log(random.path)
+  return (
+    <div className="start">
+      <button onClick={OpenSite} />
+      <a href={random.path}>Registrate Aqui</a>
+    </div>
+  )
 }
+
 export default OpenSite
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -4,27 +4,19 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-import Rand from "../components/registrylink"
 import OpenSite from "../components/regostylinks"
 
 const SecondPage = () => {
-  const { random } = OpenSite()
   return (
     <Layout>
       <SEO title="Registro" />
       <div className="header">
         <h1>PAGINA DEL LINK</h1>
       </div>
-      <div className="start">
-        <p>Registrate AQUI</p>
-      </div>
+
       <div className="kuai__amount">
-        LINK
-        <tr>
-          <a href="javascript:OpenSite()">Registrarse en Kuailiandp</a>
-        </tr>
+        <OpenSite></OpenSite>
       </div>
-      <Rand></Rand>
     </Layout>
   )
 }
